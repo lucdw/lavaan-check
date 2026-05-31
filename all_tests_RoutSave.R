65ef6be3c51f8dee54d1e354fc9aa3d7cbe95c17
Luc De Wilde <luc.dewilde@ugent.be>
2026-05-31 10:38:40 +0200

    correct check files present

--- all_tests_RoutSave.R
+++ all_tests_RoutSave.R
@@ -1,7 +1,7 @@
 if (file.exists("README.md")) setwd("tests")
 extra <- substr(Sys.info()[["sysname"]], 1, 1) # because results depend on OS
 filenames <- list.files(pattern=paste0("\\.R", extra, "out$"))
-if (length(filenames) >10L) stop(".Rxout files not present")
+if (length(filenames) < 10L) stop(".Rxout files not present")
 file.remove(list.files(pattern=paste0("\\.R", extra, "out\\.save$")))
 filenamesnew <- paste0(filenames, ".save")
 file.rename(filenames, filenamesnew)
